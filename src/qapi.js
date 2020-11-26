73ecbc5869733eb611e61cac765dfb8f1da69c82
zoevinkx <75062879+zoevinkx@users.noreply.github.com>
2020-11-26 01:06:20 -0600

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbf198cb1c8af001eb57bfc",
+				"5fbf198cb1c8af001eb57bfe"
+			]
+		},
+		"5fbf198cb1c8af001eb57bfc": {
+			"id": "5fbf198cb1c8af001eb57bfc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbf198cb1c8af001eb57bfe": {
+			"id": "5fbf198cb1c8af001eb57bfe",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
